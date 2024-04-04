295504548052d7e98fec8c90f6623723d9a89a73
Francisco Gabriel <galvesa9@gmail.com>
2024-04-04 20:40:18 -0300

    Create controllerProduto

---
+++ src/main/java/com/gestao/demo/controllers/ControllerProduto.java
@@ -0,0 +1,23 @@
+package com.gestao.demo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerProduto {
+
+    public void quantidadeCategoria(){
+
+    }
+
+    public void valormedioporcategoria(){
+
+    }
+
+    public void valorMedioDosProdutos(){
+
+    }
+
+    public void quantidadeProdutoPorCategoria(){
+
+    }
+}
