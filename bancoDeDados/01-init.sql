5ea8197752f5e2bd2adf70f092c6c2196c508915
Francisco Gabriel <galvesa9@gmail.com>
2024-04-04 20:38:49 -0300

    Update bancoDeDados

--- bancoDeDados/01-init.sql
+++ bancoDeDados/01-init.sql
@@ -1,23 +1,9 @@
-CREATE DATABASE 'estoque';
+CREATE DATABASE estoque;
 
 CREATE TABLE produto(
     id serial not null primary key,
     nome varchar(50),
     tipo varchar(50),
-    qtd int,
-    valor_uni real
-);
-
-CREATE TABLE cliente(
-    id serial not null primary key,
-    nome varchar(30),
-    
-);
-
-CREATE TABLE venda(
-    id serial not null primary key,
-    id_produto int references produto(id),
-    id_cliente int,
-    qtd int,
-    valor_total real
+    quantidade int,
+    preco real
 );
