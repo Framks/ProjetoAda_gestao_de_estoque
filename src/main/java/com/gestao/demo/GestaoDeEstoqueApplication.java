476877bb426bc22c8d914166bd7fcdb94e69d435
Francisco <galvesa9@gmail.com>
2024-04-04 08:44:41 -0300

    Create project source

---
+++ src/main/java/com/gestao/demo/GestaoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao/demo/GestaoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
