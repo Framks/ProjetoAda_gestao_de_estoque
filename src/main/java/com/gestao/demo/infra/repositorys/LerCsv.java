2356ff6e6a9a81dfbfa1821b427f442d38074c43
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 12:50:26 -0300

    implementando LerCsv.java

--- src/main/java/com/gestao/demo/infra/repositorys/LerCsv.java
+++ src/main/java/com/gestao/demo/infra/repositorys/LerCsv.java
@@ -1,5 +1,15 @@
 package com.gestao.demo.infra.repositorys;
 
+import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
 public class LerCsv {
     private String caminho;
 
@@ -7,5 +17,19 @@ public class LerCsv {
         this.caminho = caminho;
     }
 
+    public List<List<String>> getList() throws IOException {
+        List<List<String>> listTwoDemincion = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(this.caminho));
+        String linha;
+        br.readLine();
+
+        while ((linha = br.readLine()) != null){
+            String[] valores = linha.split(",");
+            listTwoDemincion.add(Arrays.stream(valores).toList());
+        }
+
+        return listTwoDemincion;
+    }
 
 }
