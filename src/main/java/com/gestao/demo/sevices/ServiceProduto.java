8f0d36c3cc8dfdc32759151d1220cf50f28d290c
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 16:45:05 -0300

    adcionando a quantidade de categorias

--- src/main/java/com/gestao/demo/sevices/ServiceProduto.java
+++ src/main/java/com/gestao/demo/sevices/ServiceProduto.java
@@ -31,7 +31,7 @@ public class ServiceProduto {
         // se não estiver no set categorias
         // pegue a quantidade de vezes que essa categoria aparece na lista e coloque no hashmap(p categoria , count das categorias)
         // set categorias adciona a categoria para futuras verificações
-        produtos.stream().forEach((p) -> {
+        produtos.forEach((p) -> {
             Long qtd;
             if (!categorias.contains(p.getCategoria())){
                 qtd = produtos.stream().filter(x -> x.getCategoria().equals(p.getCategoria())).count();
@@ -48,7 +48,10 @@ public class ServiceProduto {
     }
 
     public Integer qtdDeCat(){
-        return null;
+        List<Produto> produtos = this.repositoryProduto.list();
+        Set<String> categorias = new HashSet<>();
+        produtos.stream().filter(produto -> !categorias.contains(produto.getCategoria())).forEach(produto -> categorias.add(produto.getCategoria()));
+        return categorias.size();
     }
 
     public Double qtdMedPorPro(){
