1e1fd62f5cb77bb9f3a6043af8f06c3b613aac11
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 12:51:55 -0300

    implementando Repositoryproduto.java

--- src/main/java/com/gestao/demo/infra/repositorys/RepositoryProduto.java
+++ src/main/java/com/gestao/demo/infra/repositorys/RepositoryProduto.java
@@ -2,6 +2,7 @@ package com.gestao.demo.infra.repositorys;
 
 import com.gestao.demo.models.Produto;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,11 +15,21 @@ public class RepositoryProduto {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Produto> list(){
-        return null;
+    public List<Produto> list() {
+        String sql = "SELECT * FROM produtos";
+        RowMapper<Produto> rowMapper = ((rs, rowNum) ->
+                new Produto(rs.getLong("id"),
+                        rs.getString("nome"),
+                        rs.getString("categoria"),
+                        rs.getInt("quantidade"),
+                        rs.getDouble("preso")
+                )
+        );
+        return this.jdbcTemplate.query(sql,rowMapper);
     }
 
     public void insert(Produto produto){
-
+        String sql = "INSERT INTO produto (nome,categoria,preco,quantidade) VALUES (?,?,?,?)";
+        jdbcTemplate.update(sql,produto.getNome(),produto.getCategoria(),produto.getpreco(),produto.getQuantidade());
     }
 }
