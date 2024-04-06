c2e28b6eea70e91178b4a1258c860d99f1cf986a
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 14:06:09 -0300

    resolvendo problema de criação de tabela em banco de dados errado

--- bancoDeDados/01-init.sql
+++ bancoDeDados/01-init.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE estoque;
+\c estoque;
 
 CREATE TABLE produto(
     id serial not null primary key,
     nome varchar(50),
-    tipo varchar(50),
+    categoria varchar(50),
     quantidade int,
     preco real
 );
