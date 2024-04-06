e2e4ad9785265059fa6c617ffd2d4bc29a5acf5a
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 11:21:07 -0300

    Defining Repository

--- src/main/java/com/gestao/demo/infra/repositorys/RepositoryProduto.java
+++ src/main/java/com/gestao/demo/infra/repositorys/RepositoryProduto.java
@@ -2,9 +2,11 @@ package com.gestao.demo.infra.repositorys;
 
 import com.gestao.demo.models.Produto;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class RepositoryProduto {
     private final JdbcTemplate jdbcTemplate;
 
@@ -17,14 +19,6 @@ public class RepositoryProduto {
     }
 
     public void insert(Produto produto){
-
-    }
-
-    public void delete(Produto produto){
-
-    }
-
-    public void update(Produto produto){
 
     }
 }
