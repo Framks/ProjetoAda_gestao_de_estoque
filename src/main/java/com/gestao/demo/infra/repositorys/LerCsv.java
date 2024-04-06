f620b2483d3dd75d5d89e8ba54ce647128992e04
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 11:12:45 -0300

    Create LerCsv.java

---
+++ src/main/java/com/gestao/demo/infra/repositorys/LerCsv.java
@@ -0,0 +1,11 @@
+package com.gestao.demo.infra.repositorys;
+
+public class LerCsv {
+    private String caminho;
+
+    public LerCsv(String caminho){
+        this.caminho = caminho;
+    }
+
+
+}
