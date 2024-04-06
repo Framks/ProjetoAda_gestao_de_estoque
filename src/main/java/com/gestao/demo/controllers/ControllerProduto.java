ec6b7c00afb5cdece8bd16a32e9160e2f23dcdb1
Francisco Gabriel <galvesa9@gmail.com>
2024-04-06 11:19:30 -0300

    Defining Getmapping Controller

--- src/main/java/com/gestao/demo/controllers/ControllerProduto.java
+++ src/main/java/com/gestao/demo/controllers/ControllerProduto.java
@@ -1,23 +1,34 @@
 package com.gestao.demo.controllers;
 
+import com.gestao.demo.sevices.ServiceProduto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerProduto {
+    private final ServiceProduto serviceProduto;
 
+    public ControllerProduto(ServiceProduto serviceProduto){
+        this.serviceProduto = serviceProduto;
+    }
+
+    @GetMapping("/quantidadePorCategoria")
     public void quantidadeCategoria(){
 
     }
 
-    public void valormedioporcategoria(){
+    @GetMapping("/valorMedioPorCategoria")
+    public void valorMedioPorCategoria(){
 
     }
 
+    @GetMapping("/valorMedioDosProdutos")
     public void valorMedioDosProdutos(){
 
     }
 
-    public void quantidadeProdutoPorCategoria(){
+    @GetMapping("/quantidadeDeProdutoPorCategoria")
+    public void quantidaDeProdutoPorCategoria(){
 
     }
 }
